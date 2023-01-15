e346c6d4c42171c54e9342dccafd7614bda94960
InternetExplor <106797809+InternetExplor@users.noreply.github.com>
2023-01-15 17:38:26 +0500

    modified:   app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let item = document.querySelector('.btn'),
+let item = document.querySelector('.item'),
 items = document.querySelector('.items')
 
 
-item.addEventListener('click', ()=>{
+items.addEventListener('click', ()=>{
   for(let child of items.children){
     child.addEventListener('click', ()=>{
       child.style.display = 'none'
