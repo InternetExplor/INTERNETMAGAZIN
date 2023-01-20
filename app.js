c3f5709fbaf0ac174ef47dcd2ee2906fe6dacc94
InternetExplor <106797809+InternetExplor@users.noreply.github.com>
2023-01-20 12:56:16 +0500

    modified:   app.js 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-let item = document.querySelector('.item'),
-items = document.querySelector('.items')
-
-
-items.addEventListener('click', ()=>{
-  for(let child of items.children){
+let btns = document.querySelectorAll('.btn'),
+paket = document.querySelector('.paket'),
+market = document.querySelector('.market'),
+x = document.querySelector('.x')
+p = document.querySelector('#p')
+count = document.querySelector('.count')
+  for(let child of btns){
+    count.style.display = ('block')
     child.addEventListener('click', ()=>{
-      child.style.display = 'none'
+        console.log(child)
+        count.innerHTML = parseInt(count.innerHTML) + parseInt(market.childElementCount - 2)
+        child.value = 'delete'
+        child.style.backgroundColor  = ('red')
+        child.parentElement.style.display = ('none')
+        cloneParent = child.parentElement.cloneNode(true)
+        cloneParent.style.display = ('block')
+        paket.appendChild(cloneParent)
+        if (market.childElementCount > 0) {
+          p.style.display = ('none')
+      }
+        market.addEventListener('click', ()=>{
+          
+            paket.style.cssText = ('display:flex; flex-wrap:wrap;')
+            x.addEventListener('click', ()=>{
+                paket.style.display = ('none')           
+            })
+        })
     })
+     
   }
-})
